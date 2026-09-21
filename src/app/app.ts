c8464aacfaf3ad51e3001dbe5b7b6362dd446f65
René Trávez <rene.travez@hotmail.com>
2026-09-20 21:08:46 -0500

    migrate

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,16 @@ import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToastModule } from 'primeng/toast';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ToastModule, ConfirmDialogModule],
+  imports: [
+    RouterOutlet,
+    ToastModule,
+    ConfirmDialogModule,
+    TranslatePipe,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
